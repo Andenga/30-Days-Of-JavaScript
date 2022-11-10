1c2b4448570f22ed867d725bf2ae4b631433f416
Andenga_L <62437418+Andenga@users.noreply.github.com>
2022-11-11 00:29:32 +0300

    Day2

---
+++ Day2 - Data Types/main.js
@@ -0,0 +1,35 @@
+let challange = "30 Days Of JavaScript"
+console.log(challange)
+console.log(challange.length)
+console.log(challange.toUpperCase())
+console.log(challange.toLowerCase())
+console.log(challange.substring(3))
+console.log(challange.substring(3, 23))
+console.log(challange.includes("Script"))
+console.log(challange.split())
+console.log(challange.split(" "))
+let x = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
+console.log(x.split(","))
+console.log(challange.replace("JavaScript", "Python"))
+console.log(challange.charAt(15))
+console.log(challange.charCodeAt("J"))
+console.log(challange.indexOf("a"))
+console.log(challange.lastIndexOf("a"))
+let s = "You cannot end a sentence with because because because\
+ is a conjunction"
+ console.log(challange.indexOf("because"))
+ console.log(challange.lastIndexOf("because"))
+ console.log(challange.trim())
+ console.log(challange.startsWith(30))
+ console.log(challange.endsWith("of JavaScript"))
+console.log(challange.match("a"))
+console.log(challange.repeat(2))
+let a = "30 Days of"
+console.log(a.concat("JavaScript lol"))
+
+//Exercise: Level 2
+console.log("The quote \'There is no exercise better exercise  for the\
+heart reaching down and lifting people up.\' by John Holmes teaches us to \
+help one another")
+let f = console.log(typeof("10"))
+console.log(f == 10)
