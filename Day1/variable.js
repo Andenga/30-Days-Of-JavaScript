de83e5a33381139263f5a720e9e9e779e177be6d
Andenga_L <62437418+Andenga@users.noreply.github.com>
2022-11-10 16:54:57 +0300

    Introduction

---
+++ Day1/variable.js
@@ -0,0 +1,16 @@
+//comments can make code readable
+//Welcome to 30DaysOfjavaScript
+/*comments can make code readable, easy to reuse
+and informative */
+var x = 9
+var y = "Hello World"
+console.log(typeof(x))
+console.log(typeof(y))
+var myAge =  16
+var yourAge = 32
+var a, b, c, d 
+var e = 23
+var t = "t is a names storage location"
+console.log("I am " + myAge + " years old.")
+console.log("You are " + yourAge + " years old.")
+
