e498a222d5193622483bd9ab4adc2d5d2fcd5f10
Andenga_L <62437418+Andenga@users.noreply.github.com>
2022-11-14 01:36:58 +0300

    Update main.js

---
+++ Day3 - Booleans , Operators, Date/main.js
@@ -0,0 +1,66 @@
+let firstName = "Mapenzi"
+let lastName = "Juma"
+let country = "Tanzania"
+let city = "Arusha"
+let age = 30
+let isMarried = true
+let year = 2022
+console.log(typeof(firstName))
+console.log(typeof(age))
+console.log(typeof(isMarried))
+console.log(typeof(10) == 10)
+console.log(parseInt("9.8") == 10)
+let numb = 0
+console.log(numb == 0)
+console.log(4 > 3)
+console.log(4 >= 3)
+console.log(4 < 3)
+console.log(4 <= 3)
+console.log(4 == 4 )
+console.log(4 === 4)
+console.log(4 != 4)
+console.log(4 !== 4)
+console.log(4 != '4')
+console.log(4 == '4')
+console.log(4 === '4')
+console.log(4 > 3 && 10 < 12)
+console.log(4 > 3 && 10 > 12)
+console.log(4 > 3 || 10 < 12)
+console.log(4 > 3 || 10 > 12)
+console.log(!(4 > 3))
+console.log(!(4 < 3))
+console.log(!(false))
+console.log(!(4 > 3 && 10 < 12))
+console.log(!(4 > 3 && 10 > 12))
+console.log(!(4 === '4'))
+
+//There is no 'on' in both dragon and python
+
+
+
+/*
+
+
+
+
+
+
+/*
+
+
+Find the length of python and jargon and make a falsy comparison statement. 
+
+*/
+
+
+/*
+Use the Date object to do the following activities
+
+What is the year today?
+What is the month today as a number?
+What is the date today?
+What is the day today as a number?
+What is the hours now?
+What is the minutes now?
+Find out the numbers of seconds elapsed from January 1, 1970 to now.
+*/
