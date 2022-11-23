1c5ca4f4f56b315da71e311343d5348a2aeb3662
Andenga_L <62437418+Andenga@users.noreply.github.com>
2022-11-23 16:36:53 +0300

    .

--- Day08 - Objects/main.js
+++ Day08 - Objects/main.js
@@ -1,23 +1,16 @@
+let dog = {};
+console.log(dog)
+dog.name = 'Bruno'
+dog.legs = 4
+dog.color = 'black'
+dog.bark = 'woof wooof'
+console.log(dog)
+console.log(dog.bark)
+dog.breed = 'African dog'
+console.log(dog.breed)
 
 
-
-/*
- 
-Exercises: Level 1
-Create an empty object called dog
-Print the the dog object on the console
-Add name, legs, color, age and bark properties for the dog object. The bark property is a method which return woof woof
-Get name, legs, color, age and bark value from the dog object
-Set new properties the dog object: breed, getDogInfo
-
-
-
-
-
-Exercises: Level 2
-Find the person who has many skills in the users object.
-
-Count logged in users, count users having greater than equal to 50 points from the following object.
+//Exercises: Level 2
 const users = {
   Alex: {
     email: 'alex@alex.com',
@@ -68,98 +61,127 @@ const users = {
     isLoggedIn: false,
     points: 40
   }
-}```
+}
 
-Find people who are MERN stack developer from the users object
+//Review
+const userWithMaxSkills = users => Object.entries(users)
+.reduce((res, [username, data]) => {
+    if(data.skills.length > res.maxSkills){
+      return {
+        maxSkills: data.skills.length,
+        user: {
+          [username]: data
+        }
+      }
+    }
+    return res
+}, 
+ { 
+  maxSkills: -1,
+  user: undefined
+ }
+).user
+console.log(userWithMaxSkills(users))
+//End of review
 
-Set your name in the users object without modifying the original users object
+//Review
+//Count logged in users, count users having greater than equal to 50 points from the following object.
+//Find people who are MERN stack developer from the users object
 
-Get all keys or properties of users object
+Object.values(users).filter(user => user.points >= 50).length
+Object.values(users).filter(user => user.isLoggedIn).length
+Object.entries(users).filter(([_, user]) => user.skills.includes("MongoDB", "Express", "React", "Node")).map(([name]) => name).join(", ")
+//End of review
+//Set your name in the users object without modifying the original users object
 
-Get all the values of users object
 
+console.log(Object.keys(users))
+console.log(Object.values(users))
+
+const users_1 = [
+  {
+      _id: 'ab12ex',
+      username: 'Alex',
+      email: 'alex@alex.com',
+      password: '123123',
+      createdAt:'08/01/2020 9:00 AM',
+      isLoggedIn: false
+  },
+  {
+      _id: 'fg12cy',
+      username: 'Asab',
+      email: 'asab@asab.com',
+      password: '123456',
+      createdAt:'08/01/2020 9:30 AM',
+      isLoggedIn: true
+  },
+  {
+      _id: 'zwf8md',
+      username: 'Brook',
+      email: 'brook@brook.com',
+      password: '123111',
+      createdAt:'08/01/2020 9:45 AM',
+      isLoggedIn: true
+  },
+  {
+      _id: 'eefamr',
+      username: 'Martha',
+      email: 'martha@martha.com',
+      password: '123222',
+      createdAt:'08/01/2020 9:50 AM',
+      isLoggedIn: false
+  },
+  {
+      _id: 'ghderc',
+      username: 'Thomas',
+      email: 'thomas@thomas.com',
+      password: '123333',
+      createdAt:'08/01/2020 10:00 AM',
+      isLoggedIn: false
+  }
+  ];
+
+  const products = [
+{
+  _id: 'eedfcf',
+  name: 'mobile phone',
+  description: 'Huawei Honor',
+  price: 200,
+  ratings: [
+    { userId: 'fg12cy', rate: 5 },
+    { userId: 'zwf8md', rate: 4.5 }
+  ],
+  likes: []
+},
+{
+  _id: 'aegfal',
+  name: 'Laptop',
+  description: 'MacPro: System Darwin',
+  price: 2500,
+  ratings: [],
+  likes: ['fg12cy']
+},
+{
+  _id: 'hedfcg',
+  name: 'TV',
+  description: 'Smart TV:Procaster',
+  price: 400,
+  ratings: [{ userId: 'fg12cy', rate: 5 }],
+  likes: ['fg12cy']
+}
+]
+
+
+
+
+/*
 Use the countries object to print a country name, capital, populations and languages.
-
-
-
-
 
 
 Exercises: Level 3
 Create an object literal called personAccount. It has firstName, lastName, incomes, expenses properties and it has totalIncome, totalExpense, accountInfo,addIncome, addExpense and accountBalance methods. Incomes is a set of incomes and its description and expenses is a set of incomes and its description.
 **** Questions:2, 3 and 4 are based on the following two arrays:users and products ()
-    const users = [
-    {
-        _id: 'ab12ex',
-        username: 'Alex',
-        email: 'alex@alex.com',
-        password: '123123',
-        createdAt:'08/01/2020 9:00 AM',
-        isLoggedIn: false
-    },
-    {
-        _id: 'fg12cy',
-        username: 'Asab',
-        email: 'asab@asab.com',
-        password: '123456',
-        createdAt:'08/01/2020 9:30 AM',
-        isLoggedIn: true
-    },
-    {
-        _id: 'zwf8md',
-        username: 'Brook',
-        email: 'brook@brook.com',
-        password: '123111',
-        createdAt:'08/01/2020 9:45 AM',
-        isLoggedIn: true
-    },
-    {
-        _id: 'eefamr',
-        username: 'Martha',
-        email: 'martha@martha.com',
-        password: '123222',
-        createdAt:'08/01/2020 9:50 AM',
-        isLoggedIn: false
-    },
-    {
-        _id: 'ghderc',
-        username: 'Thomas',
-        email: 'thomas@thomas.com',
-        password: '123333',
-        createdAt:'08/01/2020 10:00 AM',
-        isLoggedIn: false
-    }
-    ];
 
-    const products = [
-  {
-    _id: 'eedfcf',
-    name: 'mobile phone',
-    description: 'Huawei Honor',
-    price: 200,
-    ratings: [
-      { userId: 'fg12cy', rate: 5 },
-      { userId: 'zwf8md', rate: 4.5 }
-    ],
-    likes: []
-  },
-  {
-    _id: 'aegfal',
-    name: 'Laptop',
-    description: 'MacPro: System Darwin',
-    price: 2500,
-    ratings: [],
-    likes: ['fg12cy']
-  },
-  {
-    _id: 'hedfcg',
-    name: 'TV',
-    description: 'Smart TV:Procaster',
-    price: 400,
-    ratings: [{ userId: 'fg12cy', rate: 5 }],
-    likes: ['fg12cy']
-  }
-]
 
 Imagine you are getting the above users collection from a MongoDB database. a. Create a function called signUp which allows user to add to the collection. If user exists, inform the user that he has already an account.
 b. Create a function called signIn which allows user to sign in to the application
