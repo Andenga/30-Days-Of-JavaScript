d1715aa7228d2b5fef5952fb8e999da94c48373b
Andenga_L <62437418+Andenga@users.noreply.github.com>
2022-11-25 03:27:35 +0300

    .

--- Day09 - Higher Order Functions/main.js
+++ Day09 - Higher Order Functions/main.js
@@ -41,36 +41,21 @@ console.log(countries.findIndex((country) => country == "Norway"))
 console.log(countries.findIndex((country) => country == "Russia"))
 
 //Exercises: Level 2
-
-
-
-
-
 /*
 Find the total price of products by chaining two or more array iterators
 (eg. arr.map(callback).filter(callback).reduce(callback))
-
 Find the sum of price of products using only reduce reduce(callback))
-
 Declare a function called categorizeCountries which returns an array of countries which have some common pattern
 (you find the countries array in this repository as countries.js(eg 'land', 'ia', 'island','stan')).
-
 Create a function which return an array of objects, which is the letter and the number of times the 
 letter use to start with a name of a country.
-
-Declare a getFirstTenCountries function and return an array of ten countries. Use different functional
- programming to work on the countries.js array
-
+Declare a getFirstTenCountries function and return an array of ten countries. Use different functional programming to work on the countries.js array
 Declare a getLastTenCountries function which which returns the last ten countries in the countries array.
-
+Find out which letter is used many times as initial for a country name from the countries array (eg. Finland, Fiji, France etc)
+Use the countries information, in the data folder. Sort countries by name, by capital, by population
 Find out which letter is used many times as initial for a country name from the countries array
  (eg. Finland, Fiji, France etc)
 
-
-
-
-
-Exercises: Level 3
 Use the countries information, in the data folder. Sort countries by name, by capital, by population
 
 *** Find the 10 most spoken languages:
