85f72631b79596642acb34cd2d19ac0d308b7e3d
Andenga_L <62437418+Andenga@users.noreply.github.com>
2022-11-25 03:30:36 +0300

    .

--- Day10 - Sets and Maps/main.js
+++ Day10 - Sets and Maps/main.js
@@ -1,23 +1,30 @@
-let emptyset = new set ()
+let emptyset = new Set ()
 console.log(emptyset)
 
 
 
 /*xercises:Level 1
-const a = [4, 5, 8, 9]
-const b = [3, 4, 5, 7]
-const countries = ['Finland', 'Sweden', 'Norway']
 
-create an empty set
 Create a set containing 0 to 10 using loop
 Remove an element from a set
 Clear a set
 Create a set of 5 string elements from array
 Create a map of countries and number of characters of a country
+
+const a = [4, 5, 8, 9]
+const b = [3, 4, 5, 7]
+const countries = ['Finland', 'Sweden', 'Norway']
+
+
+
 Exercises:Level 2
 Find a union b
 Find a intersection b
 Find a with b
+
+
+
+
 Exercises:Level 3
 How many languages are there in the countries object file.
 
