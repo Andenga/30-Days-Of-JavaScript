6a3272418bfec4137105bb1e65f5623cb941d887
Andenga_L <62437418+Andenga@users.noreply.github.com>
2022-11-29 01:56:22 +0300

    .

--- Day11 - Destructuring and Spreading/main.js
+++ Day11 - Destructuring and Spreading/main.js
@@ -1,21 +1,18 @@
-
-
-
-
-
-
-
-/*
-
-Exercises: Level 1
 const constants = [2.72, 3.14, 9.81, 37, 100]
 const countries = ['Finland', 'Estonia', 'Sweden', 'Denmark', 'Norway']
+let [e, pi, gravity, humanBodyTemp, waterBoilingTemp] = constants
+console.log(e, pi, gravity, humanBodyTemp, waterBoilingTemp)
 const rectangle = {
   width: 20,
   height: 10,
   area: 200,
   perimeter: 60
 }
+let [fin, est, sw, den, nor] = countries
+console.log(fin, est, sw, den, nor)
+let {width, height, area, perimeter} = rectangle
+console.log(width, height, area, perimeter)
+
 const users = [
 {
   name:'Brook',
@@ -60,13 +57,19 @@ const users = [
   age:20
 }
 ]
-Destructure and assign the elements of constants array to e, pi, gravity, humanBodyTemp, waterBoilingTemp.
-Destructure and assign the elements of countries array to fin, est, sw, den, nor
-Destructure the rectangle object by its properties or keys.
-Exercises: Level 2
-Iterate through the users array and get all the keys of the object using destructuring
-Find the persons who have less than two skills
-Exercises: Level 3
+//Exercises: Level 2
+for (const {name, scores, skills, age} of users){
+  console.log(name, scores, skills, age)
+}
+//Find the persons who have less than two skills
+
+//Exercises: Level 3
+
+
+
+
+/*
+
 Destructure the countries object print name, capital, population and languages of all countries
 A junior developer structure student name, skills and score in array of arrays which may not easy to read. Destructure the following array name to name, skills array to skills, scores array to scores, JavaScript score to jsScore and React score to reactScore variable in one line.
   const student = ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]]
