f890cb22fbcfcc5435e5b7dab94102631d11ffd9
Andenga_L <62437418+Andenga@users.noreply.github.com>
2022-11-29 03:57:12 +0300

    .

--- Day12 - Regular Expressions/main.js
+++ Day12 - Regular Expressions/main.js
@@ -6,7 +6,6 @@ console.log(totalAnnualIncome);
 
 
 
-
 /*
 The position of some particles on the horizontal x-axis -12, -4, -3 and -1 in the negative direction, 
 0 at origin, 4 and 8 in the positive direction. Extract these numbers and find the distance between
