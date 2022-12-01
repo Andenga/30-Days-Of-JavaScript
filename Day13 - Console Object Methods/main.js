44e86d91f4c6dacfd84085a3035c45c77e62d995
Andenga_L <62437418+Andenga@users.noreply.github.com>
2022-12-01 18:03:47 +0300

    .

--- Day13 - Console Object Methods/main.js
+++ Day13 - Console Object Methods/main.js
@@ -2,13 +2,16 @@
 
 
 
-
 /*
 Exercises:Level 1
 Display the countries array as a table
 Display the countries object as a table
 Use console.group() to group logs
+*/
 
+
+
+/*
 
 Exercises:Level 2
 10 > 2 * 10 use console.assert()
