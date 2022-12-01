3904e94ea14d046db54215d23a3da3ffb0eae025
Andenga_L <62437418+Andenga@users.noreply.github.com>
2022-12-01 18:14:35 +0300

    .

--- Day13 - Console Object Methods/main.js
+++ Day13 - Console Object Methods/main.js
@@ -1,26 +1,35 @@
-
-
-
-
 /*
 Exercises:Level 1
 Display the countries array as a table
 Display the countries object as a table
 Use console.group() to group logs
 */
-console.assert(10 > 2 * 10, "Yes it is")
-console.assert(10 > 2 * 10, "No it isn't")
+console.assert(10 > 2 * 10, "no it isn't")
+console.warn("This is a warning message")
+console.error("This is an error message ")
+
+console.time("while loop")
+let x = 5
+while (x < 2){
+    console.log(x)
+}
+console.timeEnd("while loop")
+
+console.time("for loop")
+let x = 5
+for (x = 0; x < 5; x++){
+    console.log(x)
+}
+console.timeEnd("for loop")
+
+
+
+
+
+
 
 
 /*
-
-Exercises:Level 2
-10 > 2 * 10 use console.assert()
-Write a warning message using console.warn()
-Write an error message using console.error()
-
-
-Exercises:Level 3
 Check the speed difference among the following loops: while, for, for of, forEach
 
 */
