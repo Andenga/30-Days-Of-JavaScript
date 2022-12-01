947edfb09f8c6ef93a04a1303ceabbd8ba957e89
Andenga_L <62437418+Andenga@users.noreply.github.com>
2022-12-01 19:33:13 +0300

    .

--- Day14 - Error handling/main.js
+++ Day14 - Error handling/main.js
@@ -1,1 +1,9 @@
-//Practice ...
+try {
+    let lastName = 'Yetayeh'
+    let fullName = fistName + ' ' + lastName
+  } catch (err) {
+    console.log('Name of the error', err.name)
+    console.log('Error message', err.message)
+  } finally {
+    console.log('In any case I will be executed')
+  }
