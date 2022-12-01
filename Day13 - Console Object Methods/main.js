8c29a8c19208bcb27bdb421ecdb576af389c761c
Andenga_L <62437418+Andenga@users.noreply.github.com>
2022-12-01 18:05:40 +0300

    .

--- Day13 - Console Object Methods/main.js
+++ Day13 - Console Object Methods/main.js
@@ -8,7 +8,8 @@ Display the countries array as a table
 Display the countries object as a table
 Use console.group() to group logs
 */
-
+console.assert(10 > 2 * 10, "Yes it is")
+console.assert(10 > 2 * 10, "No it isn't")
 
 
 /*
