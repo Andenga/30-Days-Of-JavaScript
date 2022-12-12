0f25d81579eeb27f794e3c5aa34bc2c6dd39aac0
Andenga_L <62437418+Andenga@users.noreply.github.com>
2022-12-12 17:06:16 +0300

    .

--- Day15 - Classes/main.js
+++ Day15 - Classes/main.js
@@ -1,25 +1,37 @@
-class Animal{
-    constructor(name, age, color, legs){
-        console.log(this)
-        this.name = name
-        this.age = age
-        this.color = color
-        this.legs = legs
+class Animal {
+    constructor(name, age, color, legs) {
+      this.name = name
+      this.age = age
+      this.color = color
+      this.legs = legs
     }
-}
+    getdetails() {
+      const details = this.name + ' ' + this.age + this.color + " " + this.legs
+      return details
+    }
+  }
+  
+  const Animal1 = new Animal('dog',  250, 'black', 4)
+  const Animal2 = new Animal('geese', 28, 'white', 2)
+  
+  console.log(person1.getFullName())
+  console.log(person2.getFullName())
 
 
+/* 
 
-
-
-
-
-/*
 Exercises Level 1
 Create an Animal class. The class will have name, age, color, legs properties and create different methods
+
 Create a Dog and Cat child class from the Animal Class.
+
+
+
 Exercises Level 2
 Override the method you create in Animal class
+
+
+
 Exercises Level 3
 Let's try to develop a program which calculate measure of central tendency of a sample(mean, median, mode) and measure of variability(range, variance, standard deviation). In addition to those measures find the min, max, count, percentile, and frequency distribution of the sample. You can create a class called Statistics and create all the functions which do statistical calculations as method for the Statistics class. Check the output below.
 ages = [31, 26, 34, 37, 27, 26, 32, 32, 26, 27, 27, 24, 32, 33, 27, 25, 26, 38, 37, 31, 34, 24, 33, 29, 26]
@@ -50,6 +62,7 @@ Variance:  17.5
 Standard Deviation:  4.2
 Frequency Distribution: [(20.0, 26), (16.0, 27), (12.0, 32), (8.0, 37), (8.0, 34), (8.0, 33), (8.0, 31), (8.0, 24), (4.0, 38), (4.0, 29), (4.0, 25)]
 Create a class called PersonAccount. It has firstname, lastname, incomes, expenses properties and it has totalIncome, totalExpense, accountInfo,addIncome, addExpense and accountBalance methods. Incomes is a set of incomes and its description and expenses is also a set of expenses and its description.
+tada CONGRATULATIONS ! tada
 
 
 */
